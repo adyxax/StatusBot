329ff67a8626e6a4c6d9201804bb0b02d74b65a2
Julien Dessaux <judessaux@gmail.com>
2010-07-25 18:19:22 +0200

    Cleaned the non working fallback function

--- statusbot.sh
+++ statusbot.sh
@@ -12,19 +12,6 @@ OS=$(uname -s)
 # Then we source the settings file
 source $BotPath/settings.sh
 
-# Fallback function {{{
-function bot_fallback ()
-{
-    trap bot_restart USR1
-    # set another trap on SIG_ERR that would just display a dzen warning and wait for a SIG_USR1 reload to avoid crashes on updates
-    while true; do
-        echo "^fg('"$CurrentFGColor"')^bg('"$CurrentBGColor"')StatusBot has detected an abnormal error. If you see this message,
-        go fix your code then send a SIG_USR1 to the statusbot to restart it." | \
-        sleep 10
-    done | dzen2 -ta l -sa l -fg "$NormalFGColor" -bg "$NormalBGColor" -fn "$Font" -x 0 -y 0 -h 16
-}
-# }}}
-
 # Restart function {{{
 function bot_restart ()
 {
@@ -34,16 +21,8 @@ function bot_restart ()
 }
 # }}}
 
-# Terminate function {{{
-function bot_terminate ()
-{
-    exit 0
-}
-# }}}
-
-trap bot_terminate KILL TERM
+trap 'exit 1' KILL TERM
 trap bot_restart USR1
-trap bot_fallback ERR
 
 coproc WorkspaceBot { workspaces_bot; }
 trayer_bot &
