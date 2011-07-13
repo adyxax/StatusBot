5a371cd7bd863954d989c63f175eada239f6d16d
Julien Dessaux <judessaux@gmail.com>
2011-07-13 20:11:59 +0200

    Cosmetics.

--- modules/cpumem.sh
+++ modules/cpumem.sh
@@ -18,7 +18,7 @@ function cpumem_bot ()
             MEMPCT=$(echo '100 * '${MEMUSED}' / '${MEMTOT} | bc)
             SWAPPCT=$(echo '100 * '${SWAPUSED}' / '${SWAPTOT} | bc)
         else
-            eval `awk '/MemTotal/   {mtotal=$2};
+            eval `awk '/^MemTotal:/   {mtotal=$2};
                        /^MemFree:/    {mfree=$2};
                        /^Buffers:/    {mbuffers=$2};
                        /^Cached:/     {mcached=$2};
@@ -31,27 +31,25 @@ function cpumem_bot ()
                              print "MEMPCT=" int(100 * mactive / mtotal) "; \
                              SWAPPCT=" int(100 * swactive / swtotal) ""}' \
                        /proc/meminfo`
-                             #print "^tw()^fg(#" mcolor "" mpct "% " $PicMem "\n"; \
-                             #SWAPPCT=" int(100 * swactive / swtotal) ""}' \
         fi
 
-            if [ "${MEMPCT}" -lt 60 ]; then
-                MEMCOLOUR='#20EE20'
-            elif [ "${MEMPCT}" -lt 80 ]; then
-                MEMCOLOUR='#DCA41C'
-            else
-                MEMCOLOUR='#EE2020'
-            fi
-            MEMBAR="$(echo ${MEMPCT}|gdbar -fg ${MEMCOLOUR} -bg '#37383a' -w 100 -h 6 -sh 16 -nonl)"
+        if [ "${MEMPCT}" -lt 60 ]; then
+            MEMCOLOUR='#20EE20'
+        elif [ "${MEMPCT}" -lt 80 ]; then
+            MEMCOLOUR='#DCA41C'
+        else
+            MEMCOLOUR='#EE2020'
+        fi
+        MEMBAR="$(echo ${MEMPCT}|gdbar -fg ${MEMCOLOUR} -bg '#37383a' -w 100 -h 6 -sh 16 -nonl)"
 
-            if [ "${SWAPPCT}" -lt 10 ]; then
-                SWAPCOLOUR='#20EE20'
-            elif [ "${SWAPPCT}" -lt 30 ]; then
-                SWAPCOLOUR='#DCA41C'
-            else
-                SWAPCOLOUR='#EE2020'
-            fi
-            SWAPBAR="$(echo ${SWAPPCT}|gdbar -fg ${SWAPCOLOUR} -bg '#37383a' -w 100 -h 6 -sh 16 -nonl)"
+        if [ "${SWAPPCT}" -lt 10 ]; then
+            SWAPCOLOUR='#20EE20'
+        elif [ "${SWAPPCT}" -lt 30 ]; then
+            SWAPCOLOUR='#DCA41C'
+        else
+            SWAPCOLOUR='#EE2020'
+        fi
+        SWAPBAR="$(echo ${SWAPPCT}|gdbar -fg ${SWAPCOLOUR} -bg '#37383a' -w 100 -h 6 -sh 16 -nonl)"
 
         echo "^tw()^fg(${MEMCOLOUR})$MEMPCT"'% '$PicMem
         echo " RAM  : ^fg(${MEMCOLOUR})$MEMPCT"'% '$MEMBAR
