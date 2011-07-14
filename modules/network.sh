ea24457fb5b4acea9803f9a815f462fbdf1843ea
Julien Dessaux <judessaux@gmail.com>
2011-07-15 00:21:31 +0200

    Greatly improved the network module's internals and its output.

--- modules/network.sh
+++ modules/network.sh
@@ -5,38 +5,39 @@ DZEN_network="dzen2 -ta r -sa l -fg $NormalFGColor -bg $NormalBGColor -fn $Font 
 function network_bot ()
 {
     trap exit USR1 TERM KILL
+    epd=0; epu=0; wpd=0; wpu=0
     while true; do
-        if [ "$OS" == "OpenBSD" ]; then
-            eval `ifstat -i em0,wpi0 0.1 1 | awk '/[^a-z][0-9\.]+/ {print "EM0DOWN=" $1 "; EM0UP=" $2 "; WPI0DOWN=" $3 "; WPI0UP=" $4}'`
-            if [ -n "$(ifconfig em0 |grep -e 'status: active')" ]; then
-                EM0TRAF="${EM0DOWN}kB/s${PicDown} ${EM0UP}kB/s${PicUp}"
-                PCIINFO="^fg($CurrentFGColor)$PicNetWired^fg() $EM0TRAF"
-            else
-                PCIINFO=""
-            fi
-            if [ -n "$(ifconfig wpi0 |grep -e 'status: active')" ]; then
-                WPI0TRAF="${WPI0DOWN}kB/s${PicDown} ${WPI0UP}kB/s${PicUp}"
-                WIFIINFO="^fg($CurrentFGColor)$PicNetWifi^fg() $WPI0TRAF"
-            else
-                WIFIINFO=""
-            fi
+        if [ -n "$(ifconfig eth0 |grep -e 'inet addr')" ]; then
+            eval `awk -v epd=$epd -v epu=$epu \
+                      '/eth0/ { ed=$2; eu=$10 ; down=ed-epd; up=eu-epu;
+                          if (down > 1048576) { down=down/1048576; du="MB" }
+                          else if (down > 1024) { down=down/1024; du="KB" }
+                          else du="B";
+                          if (up > 1048576) { up=up/1048576; uu="MB" }
+                          else if (up > 1024) { up=up/1024; uu="KB" }
+                          else uu="B" };
+                      END { printf("epd=%d; epu=%d; down=%d; up=%d; du=%s; uu=%s", ed, eu, down, up, du, uu) }' \
+                      /proc/net/dev`
+            PCIINFO="^fg($CurrentFGColor)$PicNetWired^fg() ${down} ${du}${PicDown} ${up} ${uu}${PicUp}"
         else
-            if [ -n "$(ifconfig eth0 |grep -e 'inet addr')" ]; then
-                eval `ifstat eth0 | awk '/^eth0/ {print "EM0DOWN=" $6 "; EM0UP=" $8 ""}'`
-                EM0TRAF="$((EM0DOWN / 1024))kB/s${PicDown} $((EM0UP / 1024))kB/s${PicUp}"
-                PCIINFO="^fg($CurrentFGColor)$PicNetWired^fg() $EM0TRAF"
-            else
-                PCIINFO=""
-            fi
-            if [ -n "$(ifconfig wlan0 |grep -e 'inet addr')" ]; then
-                eval `ifstat wlan0 | awk '/^wlan0/ {print "WPI0DOWN=" $6 "; WPI0UP=" $8 ""}'`
-                WPI0TRAF="$((WPI0DOWN / 1024))kB/s${PicDown} $((WPI0UP / 1024))kB/s${PicUp}"
-                WIFIINFO="^fg($CurrentFGColor)$PicNetWifi^fg() $WPI0TRAF"
-            else
-                WIFIINFO=""
-            fi
+            PCIINFO=""
         fi
-        echo "$PCIINFO $WIFIINFO "
+        if [ -n "$(ifconfig wlan0 |grep -e 'inet addr')" ]; then
+            eval `awk -v wpd=$wpd -v wpu=$wpu \
+                      '/wlan0/ { wd=$2; wu=$10 ; down=wd-wpd; up=wu-wpu;
+                          if (down > 1048576) { down=down/1048576; du="MB" }
+                          else if (down > 1024) { down=down/1024; du="KB" }
+                          else du="B";
+                          if (up > 1048576) { up=up/1048576; uu="MB" }
+                          else if (up > 1024) { up=up/1024; uu="KB" }
+                          else uu="B" };
+                      END { printf("wpd=%d; wpu=%d; down=%d; up=%d; du=%s; uu=%s", wd, wu, down, up, du, uu) }' \
+                      /proc/net/dev`
+            WIFIINFO="^fg($CurrentFGColor)$PicNetWifi^fg() ${down} ${du}${PicDown} ${up} ${uu}${PicUp}"
+        else
+            WIFIINFO=""
+        fi
+        echo "${PCIINFO} ${WIFIINFO}"
         sleep 1
     done | $DZEN_network
 }
